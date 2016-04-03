1d2ce53b7fc8282a02b385bfdca1ae5ff139e9a6
Himanshu Attri <attri.him@gmail.com>
2016-04-04 00:38:08 +0530

    Few Changes Done

--- app/src/main/java/com/himanshu/app/wifi/MainActivity.java
+++ app/src/main/java/com/himanshu/app/wifi/MainActivity.java
@@ -10,7 +10,9 @@ import android.os.Environment;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.Toolbar;
+import android.text.method.ScrollingMovementMethod;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -21,11 +23,14 @@ import android.widget.Toast;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.text.DecimalFormat;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity  {
     TextView[] txv = new TextView[10];
-    ListView lv;
+    //TextView thead =(TextView)findViewById(R.id.tv_heading);
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,13 +38,14 @@ public class MainActivity extends AppCompatActivity  {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        lv=(ListView) findViewById(R.id.lv1);
+
 
         txv[0]=(TextView) findViewById(R.id.tv1);
        /* txv[1]=(TextView) findViewById(R.id.tv2);
         txv[2]=(TextView) findViewById(R.id.tv3);
         txv[3]=(TextView) findViewById(R.id.tv4);*/
        // txv5=(TextView) findViewById(R.id.tv5);
+        //thead.setText("List Of WiFi Available !");
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -62,27 +68,14 @@ public class MainActivity extends AppCompatActivity  {
                     double exp = (27.55 - (20 * Math.log10(scanresult.frequency)) + Math.abs(scanresult.level)) / 20.0;
 
                     Math.pow(10.0, exp);
+                    txv[0].setMovementMethod(new ScrollingMovementMethod());
+                    double dis=(Math.pow(10.0, exp)*100.0)/100.0;
+                    DecimalFormat df = new DecimalFormat("###.##");
 
-                    txv[0].append("RSSI: " + scanresult.level + "\n" + "Frequency: " + scanresult.frequency + "\n" + "BSSID: " + scanresult.BSSID + "\n" + "Capability: " + scanresult.capabilities + "\n" + scanresult.SSID + "\n" + scanresult.timestamp / 60000000 +  "\n"+"Approx Distance Of Access Point: "+Math.pow(10.0, exp)+" meters." +"\n\n");
+                    txv[0].append ( "BSSID: " + scanresult.BSSID + "\n");
+                    txv[0].append("RSSI: " + scanresult.level + "\n" + "Frequency: " + scanresult.frequency + "\n"+ "Capability: " + scanresult.capabilities + "\n" + scanresult.SSID + "\n" + scanresult.timestamp / 60000000 +  "\n"+"Approx Distance Of Access Point: "+df.format(dis)+" meters." +"\n\n");
 
-                    try
-                    {
-                        File root = new File(Environment.getExternalStorageDirectory().getAbsolutePath());
-                        if (!root.exists()) {
-                            root.mkdirs();
-                        }
-                        File gpxfile = new File(root, "safe.txt");
-                        FileWriter writer = new FileWriter(gpxfile);
-                        writer.append("AC");
-                        writer.flush();
-                        writer.close();
-                    //    Toast.makeText( "Saved", Toast.LENGTH_SHORT).show();
-                    }
-                    catch(IOException e)
-                    {
-                        e.printStackTrace();
 
-                    }
 
 
                 }
