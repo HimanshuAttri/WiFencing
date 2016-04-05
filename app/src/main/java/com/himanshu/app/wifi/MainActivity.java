f3ec25b3be82414cfea316888526c89dfe7e8fa6
Himanshu Attri <attri.him@gmail.com>
2016-04-06 00:52:58 +0530

    adding location bssid based

--- app/src/main/java/com/himanshu/app/wifi/MainActivity.java
+++ app/src/main/java/com/himanshu/app/wifi/MainActivity.java
@@ -1,7 +1,11 @@
 package com.himanshu.app.wifi;
 
 import android.app.ListActivity;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
+import android.content.Intent;
 import android.content.IntentFilter;
 import android.net.wifi.ScanResult;
 import android.net.wifi.WifiManager;
@@ -9,6 +13,7 @@ import android.os.Bundle;
 import android.os.Environment;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.Toolbar;
@@ -26,10 +31,12 @@ import java.io.IOException;
 import java.text.DecimalFormat;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity  {
+public class MainActivity extends AppCompatActivity {
+    NotificationManager manager;
+    Notification myNotication;
+    private static final int NOTIFY_ME_ID=1337;
     TextView[] txv = new TextView[10];
     //TextView thead =(TextView)findViewById(R.id.tv_heading);
-
 
 
     @Override
@@ -41,53 +48,74 @@ public class MainActivity extends AppCompatActivity  {
         //
 
 
-        txv[0]=(TextView) findViewById(R.id.tv1);
+        txv[0] = (TextView) findViewById(R.id.tv1);
        /* txv[1]=(TextView) findViewById(R.id.tv2);
         txv[2]=(TextView) findViewById(R.id.tv3);
         txv[3]=(TextView) findViewById(R.id.tv4);*/
-       // txv5=(TextView) findViewById(R.id.tv5);
+        // txv5=(TextView) findViewById(R.id.tv5);
         //thead.setText("List Of WiFi Available !");
 
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
 
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "List of available Wi-Fi", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-                WifiManager wManager;
-                List<ScanResult> wifiList;
 
 
-                wManager = (WifiManager) getSystemService(Context.WIFI_SERVICE);
-
-                wifiList = wManager.getScanResults();
-                txv[0].setText("");
-                new IntentFilter(wManager.SCAN_RESULTS_AVAILABLE_ACTION);
-                wManager.startScan();
-                for (int i=0; i<wifiList.size(); i++){
-                    ScanResult scanresult = wManager.getScanResults().get(i);
-                    double exp = (27.55 - (20 * Math.log10(scanresult.frequency)) + Math.abs(scanresult.level)) / 20.0;
-
-                    Math.pow(10.0, exp);
-                    txv[0].setMovementMethod(new ScrollingMovementMethod());
-                    double dis=(Math.pow(10.0, exp)*100.0)/100.0;
-                    DecimalFormat df = new DecimalFormat("###.##");
-
-                    txv[0].append ( "BSSID: " + scanresult.BSSID + "\n");
-                    txv[0].append("RSSI: " + scanresult.level + "\n" + "Frequency: " + scanresult.frequency + "\n"+ "Capability: " + scanresult.capabilities + "\n" + scanresult.SSID + "\n" + scanresult.timestamp / 60000000 +  "\n"+"Approx Distance Of Access Point: "+df.format(dis)+" m." +"\n\n");
+                    RunScan();
 
 
 
 
-                }
 
 
             }
-        });
+
+
+
+
+    public void RunScan() {
+        WifiManager wManager;
+        List<ScanResult> wifiList;
+
+
+        wManager = (WifiManager) getSystemService(Context.WIFI_SERVICE);
+
+        wifiList = wManager.getScanResults();
+        txv[0].setText("");
+        new IntentFilter(wManager.SCAN_RESULTS_AVAILABLE_ACTION);
+        wManager.startScan();
+        for (int i = 0; i < wifiList.size(); i++) {
+            ScanResult scanresult = wManager.getScanResults().get(i);
+            double exp = (27.55 - (20 * Math.log10(scanresult.frequency)) + Math.abs(scanresult.level)) / 20.0;
+
+            Math.pow(10.0, exp);
+            txv[0].setMovementMethod(new ScrollingMovementMethod());
+            double dis = (Math.pow(10.0, exp) * 100.0) / 100.0;
+            DecimalFormat df = new DecimalFormat("###.##");
+            String id=scanresult.BSSID.toString();
+           if(id=="30:b5:c2:cf:7f:b0")
+            {
+                manager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+                Notification.Builder builder = new Notification.Builder(MainActivity.this);
+                builder.setAutoCancel(false);
+                builder.setTicker("this is ticker text");
+                builder.setContentTitle("WhatsApp Notification");
+                builder.setContentText("You have a new message");
+                builder.setSmallIcon(R.drawable.ifo);
+                // builder.setContentIntent(pendingIntent);
+                builder.setOngoing(true);
+                builder.setSubText("This is subtext...");   //API level 16
+                builder.setNumber(100);
+                builder.build();
+                myNotication = builder.getNotification();
+                manager.notify(11, myNotication);
+                break;
+            }
+
+            txv[0].append("BSSID: " + scanresult.BSSID + "\n");
+            txv[0].append("RSSI: " + scanresult.level + "\n" + "Frequency: " + scanresult.frequency + "\n" + "Capability: " + scanresult.capabilities + "\n" + scanresult.SSID + "\n" + scanresult.timestamp / 60000000 + "\n" + "Approx Distance Of Access Point: " + df.format(dis) + " m." + "\n\n");
+
+
+        }
     }
 
-
-    }
-
+}
